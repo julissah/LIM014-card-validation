0a56673a3e791cc07f48646049d327bc60f3df44
Julissa <julissa.2606@gmail.com>
2021-01-28 09:45:21 -0500

    Card validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import validator from './validator.js';
+// creamos una variable para almacenar el id del boton con el metodo getElementById
+window.addEventListener("load", cargaPagina);
+const btnHome = document.getElementById('btnHome');
 
-console.log(validator);
+// Usamos el metodo addEventListener para darle funcionalidad al boton, donde pondremos 2 parametros
+// El 1er parámetro es un string (evento) al seleccionar :click
+// El 2do parámetro es la función
+
+function cargaPagina() {
+    const btnBuy = document.getElementById('btnBuy');
+    btnBuy.addEventListener('click',buy);
+    
+}
+
+btnHome.addEventListener('click',home);
+
+function buy() {
+    let numberCard = document.getElementById('numberCard').value;
+    let isValid = validator.isValid(numberCard);
+    let maskify = validator.maskify(numberCard);
+    let modal = document.getElementById("myModal");
+    let modal1 = document.getElementById("myModal1");
+    // let span = document.getElementsByClassName("close")[0];
+
+    if (numberCard){
+        
+        if(isValid == true){
+            
+            let inputNombre = document.getElementById("numberCard");
+            inputNombre.value = maskify;
+            document.getElementById('bEnviar').classList.add('hide');
+            document.getElementById('bInicio').classList.remove('hide');
+            modal.style.display = "block";
+
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                  modal.style.display = "none";
+                }
+              }
+        }
+        else{
+            modal1.style.display = "block";
+            window.onclick = function(event) {
+                if (event.target == modal1) {
+                  modal1.style.display = "none";
+                }
+              }
+        }
+    }
+    else{
+        alert('Ingrese el número de tarjeta');
+    }
+}
+
+function home(){
+
+    document.getElementsByTagName("input")[0].value = "";
+    document.getElementsByTagName("input")[1].value = "";
+    document.getElementById('bEnviar').classList.remove('hide');
+    document.getElementById('bInicio').classList.add('hide');
+}
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,94 @@
 const validator = {
-  // ...
+  
+  isValid:(numberCard)=>{
+    
+    // implementamos el metodo split para convertir el valor de un string en un array
+    let reverseCard = numberCard.split('').reverse();
+
+    let result1 = '';
+    let result2 = '';
+    let result3 = '';
+    let array1 = [];
+    let array2 = [];
+    let array3 = [];
+    let arrayConcat = [];
+    let sum = 0;
+    // let numbers = [];
+    let total = 0;
+
+    for (let h=0; h<reverseCard.length;h+=2 ){
+
+        result1 += reverseCard[h];
+        array1 = result1.split('');
+    }
+
+    for (let i = 1; i<reverseCard.length; i+=2){
+
+        reverseCard[i] *= 2;
+        if(reverseCard[i] > 9){
+
+            let numberTwoDigit = reverseCard.toString();
+            let addNumber = numberTwoDigit.split(',')[i];
+            let num1 = addNumber.charAt(0);
+            let num2 = addNumber.charAt(1);
+            let sum = parseInt(num1)+parseInt(num2);
+            result2 += sum;
+            array2 = result2.split('');
+        }
+        else {
+          result3 += reverseCard[i];
+          array3 = result3.split('');
+        }
+    }
+
+    // console.log('array 1: ' + array1);
+    // console.log('array 2: ' + array2);
+    // console.log('array 3: ' + array3);
+
+    arrayConcat = array1.concat(array2,array3);
+    
+    // for(let n=0; n<arrayConcat.length; n++){
+      
+    //   // console.log(arrayConcat[n]);
+    //   numbers += arrayConcat[n];
+    //   console.log(numbers);
+
+    // }
+    
+    arrayConcat.forEach(function(n){
+      sum += parseInt(n);
+    });
+
+    if(sum % 10 == 0){
+      total = true;
+    }
+    else{
+      total = false;
+    }
+
+    return total;
+  },
+
+  maskify:(numberCard)=>{
+    let forDigit = numberCard.slice(numberCard.length-4);
+    let hideDigit = numberCard.split('');
+    let replaceDigit = '';
+    let addReplace = '';
+    let ok = '';
+
+    for (let h=0; h<hideDigit.length-4; h++){
+
+      replaceDigit += '#';
+      
+    }
+    
+    addReplace = replaceDigit.concat(forDigit);
+    ok = addReplace.toString();
+    // console.log(addReplace);
+    // console.log(ok);
+
+    return ok;
+  },
 };
 
 export default validator;
